5e9960d7cc484d7334d406641e580c0dffe65ed5
HJuls2 <giuliobaronti@gmail.com>
2019-01-22 17:10:58 +0100

    workink arch post ricevimento

--- cnn_arch.py
+++ cnn_arch.py
@@ -1,93 +1,93 @@
-from keras.models import Model,Sequential
-from keras.layers import Conv2D, MaxPooling2D,AveragePooling2D,Input,concatenate,Flatten
-from keras.layers.core import Dense
+import tensorflow as tf
+from tensorflow.keras import layers
 
 def main():
     
-    inputs=Input(shape=(160, 60, 3))
+    inputs=layers.Input(shape=(160, 60, 3))
     
     '''
     # shared convolutional layer
-    c1 = Conv2D(20, (5,5), strides=(2,2), activation='relu', use_bias=False)(inputs)
-    mp1 = MaxPooling2D()(c1)
-    c2 = Conv2D(32, (5,5), strides=(2,2), activation='relu',use_bias=False)(mp1)
-    avg2 = AveragePooling2D()(c2)
-    c3 = Conv2D(50, (5,5), strides=(2,2), activation='relu',use_bias=False)(avg2)
-    avg3=AveragePooling2D()(c3)
+    c1 = layers.Conv2D(20, (5,5),   strides=(2,2),padding='same',activation='relu', use_bias=False)(inputs)
+    mp1 = layers.MaxPooling2D()(c1)
+    c2 = layers.Conv2D(32, (5,5), strides=(2,2),padding='same', activation='relu',use_bias=False)(mp1)
+    avg2 = layers.AveragePooling2D()(c2)
+    c3 = layers.Conv2D(50, (5,5), strides=(2,2),padding='same', activation='relu',use_bias=False)(avg2)
+    avg3=layers.AveragePooling2D()(c3)
     '''
+    
 
     # 4 separate convolutional layers
     
     # Chain A
-    a_c1 = Conv2D(20, (5,5), strides=(2,2), activation='relu', use_bias=False)(inputs)
-    a_mp1 = MaxPooling2D(strides=(2,2))(a_c1)
-    a_c2 = Conv2D(32, (5,5), strides=(2,2), activation='relu',use_bias=False)(a_mp1)
-    a_avg2 = AveragePooling2D(strides=(2,2))(a_c2)
-    a_c3 = Conv2D(50, (5,5), strides=(2,2), activation='relu',use_bias=False)(a_avg2)
-    a_avg3=AveragePooling2D(strides=(2,2))(a_c3)
+    a_c1 = layers.Conv2D(20, (5,5), strides=(2,2),padding='same', activation='relu', use_bias=False)(inputs)
+    a_mp1 = layers.MaxPooling2D()(a_c1)
+    a_c2 = layers.Conv2D(32, (5,5), strides=(2,2),padding='same', activation='relu',use_bias=False)(a_mp1)
+    a_avg2 = layers.AveragePooling2D()(a_c2)
+    a_c3 = layers.Conv2D(50, (5,5), strides=(2,2),padding='same',  activation='relu',use_bias=False)(a_avg2)
+    a_avg3=layers.AveragePooling2D()(a_c3)
     
     # Chain B
-    b_c1 = Conv2D(20, (5,5), strides=(2,2), activation='relu', use_bias=False)(inputs)
-    b_mp1 = MaxPooling2D(strides=(2,2))(b_c1)
-    b_c2 = Conv2D(32, (5,5), strides=(2,2), activation='relu',use_bias=False)(b_mp1)
-    b_avg2 = AveragePooling2D(strides=(2,2))(b_c2)
-    b_c3 = Conv2D(50, (5,5), strides=(2,2), activation='relu',use_bias=False)(b_avg2)
-    b_avg3=AveragePooling2D(strides=(2,2))(b_c3)
+    b_c1 = layers.Conv2D(20, (5,5), strides=(2,2), padding='same',activation='relu', use_bias=False)(inputs)
+    b_mp1 = layers.MaxPooling2D(strides=(2,2))(b_c1)
+    b_c2 = layers.Conv2D(32, (5,5), strides=(2,2), padding='same',activation='relu',use_bias=False)(b_mp1)
+    b_avg2 = layers.AveragePooling2D(strides=(2,2))(b_c2)
+    b_c3 = layers.Conv2D(50, (5,5), strides=(2,2),padding='same', activation='relu',use_bias=False)(b_avg2)
+    b_avg3=layers.AveragePooling2D(strides=(2,2))(b_c3)
     
     # Chain C
-    c_c1 = Conv2D(20, (5,5), strides=(2,2), activation='relu', use_bias=False)(inputs)
-    c_mp1 = MaxPooling2D(strides=(2,2))(c_c1)
-    c_c2 = Conv2D(32, (5,5), strides=(2,2), activation='relu',use_bias=False)(c_mp1)
-    c_avg2 = AveragePooling2D(strides=(2,2))(c_c2)
-    c_c3 = Conv2D(50, (5,5), strides=(2,2), activation='relu',use_bias=False)(c_avg2)
-    c_avg3=AveragePooling2D(strides=(2,2))(c_c3)
+    c_c1 = layers.Conv2D(20, (5,5), strides=(2,2),padding='same',activation='relu', use_bias=False)(inputs)
+    c_mp1 = layers.MaxPooling2D(strides=(2,2))(c_c1)
+    c_c2 = layers.Conv2D(32, (5,5), strides=(2,2),padding='same', activation='relu',use_bias=False)(c_mp1)
+    c_avg2 = layers.AveragePooling2D(strides=(2,2))(c_c2)
+    c_c3 = layers.Conv2D(50, (5,5), strides=(2,2),padding='same', activation='relu',use_bias=False)(c_avg2)
+    c_avg3=layers.AveragePooling2D(strides=(2,2))(c_c3)
     
     # Chain D
-    d_c1 = Conv2D(20, (5,5), strides=(2,2), activation='relu', use_bias=False)(inputs)
-    d_mp1 = MaxPooling2D(strides=(2,2))(d_c1)
-    d_c2 = Conv2D(32, (5,5), strides=(2,2), activation='relu',use_bias=False)(d_mp1)
-    d_avg2 = AveragePooling2D(strides=(2,2))(d_c2)
-    d_c3 = Conv2D(50, (5,5), strides=(2,2), activation='relu',use_bias=False)(d_avg2)
-    d_avg3=AveragePooling2D(strides=(2,2))(d_c3)
+    d_c1 = layers.Conv2D(20, (5,5), strides=(2,2),padding='same', activation='relu', use_bias=False)(inputs)
+    d_mp1 = layers.MaxPooling2D(strides=(2,2))(d_c1)
+    d_c2 = layers.Conv2D(32, (5,5), strides=(2,2),padding='same', activation='relu',use_bias=False)(d_mp1)
+    d_avg2 = layers.AveragePooling2D(strides=(2,2))(d_c2)
+    d_c3 = layers.Conv2D(50, (5,5), strides=(2,2),padding='same', activation='relu',use_bias=False)(d_avg2)
+    d_avg3=layers.AveragePooling2D(strides=(2,2))(d_c3)
     
     # Concat & flatten layers
     
     #Chain A
-    a_x=concatenate([a_avg3,b_avg3])
+    a_x=layers.concatenate([a_avg3,b_avg3])
     
     #Chain B
-    b_x=concatenate([a_avg3,b_avg3,c_avg3])
+    b_x=layers.concatenate([a_avg3,b_avg3,c_avg3])
     
     #Chain C
-    c_x=concatenate([b_avg3,c_avg3,d_avg3])
+    c_x=layers.concatenate([b_avg3,c_avg3,d_avg3])
     
     #Chain D
-    d_x=concatenate([c_avg3,d_avg3])
+    d_x=layers.concatenate([c_avg3,d_avg3])
     
-    a_fl = Flatten()(a_x)
-    b_fl = Flatten()(b_x)
-    c_fl = Flatten()(c_x)
-    d_fl = Flatten()(d_x)
+    a_fl = layers.Flatten()(a_x)
+    b_fl = layers.Flatten()(b_x)
+    c_fl = layers.Flatten()(c_x)
+    d_fl = layers.Flatten()(d_x)
     
     
     # Fully connnected layers
     
     
     #Chain A
-    a_fc1 = Dense(512, activation='relu', use_bias=None)(a_fl)
-    a_fc2 = Dense(512,activation='softmax', use_bias=None)(a_fc1)
+    a_fc1 = layers.Dense(512, activation='relu', use_bias=None)(a_fl)
+    a_fc2 = layers.Dense(512,activation='softmax', use_bias=None)(a_fc1)
     
     #Chain B
-    b_fc1 = Dense(512, activation='relu', use_bias=None)(b_fl)
-    b_fc2 = Dense(512, activation='softmax', use_bias=None)(b_fc1)
+    b_fc1 = layers.Dense(512, activation='relu', use_bias=None)(b_fl)
+    b_fc2 = layers.Dense(512, activation='softmax', use_bias=None)(b_fc1)
     
     #Chain C
-    c_fc1 = Dense(512, activation='relu', use_bias=None)(c_fl)
-    c_fc2 = Dense(512, activation='softmax', use_bias=None)(c_fc1)
+    c_fc1 = layers.Dense(512, activation='relu', use_bias=None)(c_fl)
+    c_fc2 = layers.Dense(512, activation='softmax', use_bias=None)(c_fc1)
     
     #Chain D
-    d_fc1 = Dense(512, activation='relu', use_bias=None)(d_fl)
-    d_fc2 = Dense(512, activation='softmax', use_bias=None)(d_fc1)
+    d_fc1 = layers.Dense(512, activation='relu', use_bias=None)(d_fl)
+    d_fc2 = layers.Dense(512, activation='softmax', use_bias=None)(d_fc1)
     
     
     
