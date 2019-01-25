c227102e0cf9558434bb0fa2f9ba10def9ff583b
mauroliuzzo <mauro.liuzzo@hotmail.it>
2019-01-25 18:35:23 +0100

    added print in opencv_try

--- opencv_try.py
+++ opencv_try.py
@@ -3,15 +3,14 @@ import sys
 
 
 def main():
-    path=sys.argv[1]
+    path = sys.argv[1]
     #filename=sys.argv[2]
-    im=cv2.imread(path,0)
+    im = cv2.imread(path, 0)
     cv2.namedWindow('image',cv2.WINDOW_NORMAL)
     cv2.imshow('image',im)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
-    
+    print im.shape
     
 
     
